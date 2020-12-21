ea1d1bbde736b5f8ccdcc10526f7a50949464bf8
Yilei Yu <yuyilei217@gmail.com>
2020-12-21 15:33:42 -0800

    new files

---
+++ Python_practice.py
@@ -0,0 +1,6 @@
+# Import the datetime class from the datetime module.
+import datetime
+# Use the now() attribute on the datetime class to get the present time.
+now = datetime.datetime.now()
+# Print the present time.
+print("The time right now is ", now)
